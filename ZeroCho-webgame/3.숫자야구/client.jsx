95f90240ca17058007a370cf08a95da96e258ef4
parkJi-yun <nmjk0123@naver.com>
2022-12-13 21:45:30 +0900

    NumberBaseballClass END

---
+++ ZeroCho-webgame/3.숫자야구/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import NumberBaseballClass from './NumberBaseballClass';
+
+ReactDOM.render(<NumberBaseballClass />, document.querySelector('#root'));
