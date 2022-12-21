5a83a2f06afa7b3aab49061dbea324099857f9ea
parkJi-yun <nmjk0123@naver.com>
2022-12-21 16:28:54 +0900

    RSPClass END

--- ZeroCho-webgame/5.가위바위보/RSPClass.jsx
+++ ZeroCho-webgame/5.가위바위보/RSPClass.jsx
@@ -27,21 +27,64 @@ const computerChoice = (imgCoord) => {
 class RSPClass extends Component {
     state = {
         result: '',
-        imgCoord: 0,
+        imgCoord: rspCoords.바위,
         score: 0,
     };
+    
+    interval;
 
     componentDidMount() { // 컴포넌트가 첫 렌더링된 후, 여기에 비동기 요청
-
-    }
-
-    componentDidUpdate() { // 리렌더링 후
-
+        this.interval = setInterval(this.changeHand, 100);
     }
 
     componentWillUnmount() { // 컴포넌트가 제거되기 직전, 비동기 요청 정리
-
+        clearInterval(this.interval);
     }
+
+    changeHand = () => {
+        const {imgCoord} = this.state;
+        if (imgCoord === rspCoords.바위) {
+            this.setState({
+                imgCoord: rspCoords.가위,
+            });
+        } else if (imgCoord === rspCoords.가위) {
+            this.setState({
+                imgCoord: rspCoords.보,
+            });
+        } else if (imgCoord === rspCoords.보) {
+            this.setState({
+                imgCoord: rspCoords.바위,
+            });
+        }
+    };
+
+    onClickBtn = (choice) => () => {
+        const {imgCoord} = this.state;
+        clearInterval(this.interval);
+        const myScore = scores[choice];
+        const cpuScore = scores[computerChoice(imgCoord)];
+        const diff = myScore - cpuScore;
+        if (diff === 0) {
+            this.setState({result: '비겼습니다!'});
+        } else if ([-1, 2].includes(diff)) {
+            this.setState((prevState) => {
+                return {
+                    result: '이겼습니다!',
+                    score: prevState.score + 1
+                };
+            });
+        } else {
+            this.setState((prevState) => {
+                return {
+                    result: '졌습니다!',
+                    score: prevState.score - 1
+                };
+            });
+        }
+        setTimeout(() => {
+            this.interval = setInterval(this.changeHand, 100);
+        }, 1000);
+    };
 
     render() {
         const { result, score, imgCoord } = this.state;
