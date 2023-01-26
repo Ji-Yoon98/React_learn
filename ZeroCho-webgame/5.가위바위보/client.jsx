08f586d3069e9532a8fdf8aa4b7400b1b7af875f
parkJi-yun <nmjk0123@naver.com>
2023-01-27 00:48:28 +0900

    5-6 end

--- ZeroCho-webgame/5.가위바위보/client.jsx
+++ ZeroCho-webgame/5.가위바위보/client.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import RSPClass from './RSPClass.jsx';
+import RSP from './RSP.jsx';
 
-ReactDOM.render(<RSPClass />, document.querySelector('#root'));
+ReactDOM.render(<RSP />, document.querySelector('#root'));
